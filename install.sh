f652ea5f45a84deb086ed929f571a0d712972981
Oriol Garcia <oriol.garcia@atresmediahf.com>
2018-01-31 16:18:54 +0100

    Changes the links path in init.sh

--- install.sh
+++ install.sh
@@ -28,29 +28,28 @@ git clone https://github.com/oriolgg/dotfiles ~/.dotfiles --recursive
 # Change the default shell
 chsh -s /bin/zsh
 
-ln -s .dotfiles/agignore .agignore
-ln -s .dotfiles/applescript .applescript
-ln -s .dotfiles/bash/bash-alias .bash-alias
-ln -s .dotfiles/bash/bashrc .bashrc
-ln -s .dotfiles/bash/bashrc-osx .bashrc-osx
-ln -s .dotfiles/bash/profile .profile
-ln -s .dotfiles/bin .bin
-ln -s .dotfiles/ctags .ctags
-ln -s .dotfiles/git/gitconfig .gitconfig
-ln -s .dotfiles/git/gitignore_global .gitignore_global
-ln -s .dotfiles/aliases .aliases
-ln -s .dotfiles/tmux/tmux .tmux
-ln -s .dotfiles/tmux/tmux-osx.conf .tmux-osx.conf
-ln -s .dotfiles/tmux/tmux-vim-select-pane /usr/local/bin/tmux-vim-select-pane
-ln -s .dotfiles/tmux/tmux.conf .tmux.conf
-ln -s .dotfiles/vim .vim
-ln -s .dotfiles/vim/gvimrc .gvimrc
-ln -s .dotfiles/vim/vimrc .vimrc
-ln -s .dotfiles/vim/xvimrc .xvimrc
-ln -s .dotfiles/zsh/oh-my-zsh .oh-my-zsh
-ln -s .dotfiles/zsh/oh-my-zsh .oh-my-zsh
-ln -s .dotfiles/zsh/zshrc .zshrc
-ln -s .dotfiles/zsh/zshrc-osx .zshrc-osx
+ln -s ~/.dotfiles/agignore ~/.agignore
+ln -s ~/.dotfiles/applescript ~/.applescript
+ln -s ~/.dotfiles/bash/bash-alias ~/.bash-alias
+ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash/bashrc-osx ~/.bashrc-osx
+ln -s ~/.dotfiles/bash/profile ~/.profile
+ln -s ~/.dotfiles/bin ~/.bin
+ln -s ~/.dotfiles/ctags ~/.ctags
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/aliases ~/.aliases
+ln -s ~/.dotfiles/tmux/tmux ~/.tmux
+ln -s ~/.dotfiles/tmux/tmux-osx.conf ~/.tmux-osx.conf
+ln -s ~/.dotfiles/tmux/tmux-vim-select-pane /usr/local/bin/tmux-vim-select-pane
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/xvimrc ~/.xvimrc
+ln -s ~/.dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/zshrc-osx ~/.zshrc-osx
 
 # Set default osx defaults
 /bin/bash ~/.dotfiles/osx-defaults.sh
