53fd4ffb6ccc282138ff546619a7a78f4abafa18
Oriol Garcia <oriol.garcia@atresmediahf.com>
2018-02-05 17:31:28 +0100

    Scp a file to every instance of a aws environment

--- zsh/shell-functions.sh
+++ zsh/shell-functions.sh
@@ -63,6 +63,53 @@ function ssh_aws {
     fi
 }
 
+function scp_aws {
+    session=$1
+    environment=$2
+    filePath=$3
+    fileFrom=${filePath%:*}
+    pathTo=${filePath##*:}
+
+    vpn_disconnect_all
+    vpn_connect "$1vpn"
+
+    instances=$(aws ec2 describe-instances --profile $session --region eu-west-1 --filter "Name=key-name,Values=$session-$environment" "Name=instance-state-name,Values=running")
+
+    instanceLineNumbers=($((grep -n "\"Instances\"" | cut -d : -f 1) <<< "$instances"))
+
+    total=${#instanceLineNumbers[*]}
+    if [ $total -eq 0 ]; then
+        echo "There is no instance with the key name $session-$environment running"
+    fi
+
+    if [ "${instanceLineNumbers[0]}" = "" ]; then
+        init=1
+    else
+        init=0
+    fi
+    total2=$((total+init))
+    for (( i=$init; i<$total2; i++ )) do 
+        if [ "${instanceLineNumbers[($i + 1)]}" = "" ]; then
+            currentInstance=s$(echo "$instances" | awk 'NR >= '${instanceLineNumbers[$i]})
+        else
+            currentInstance=s$(echo "$instances" | awk 'NR >= '${instanceLineNumbers[$i]}' && NR <= '${instanceLineNumbers[($i + 1)]})
+        fi
+
+        privateIpAddress=$((grep "\"PrivateIpAddress\"" | head -1) <<< "$currentInstance")
+        privateIpAddress="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$privateIpAddress")"
+
+        instanceType=$((grep "\"InstanceType\"" | sed 's: ::g' | sed 's/,$//' | uniq | head -1 | cut -d ':' -f 2 | sed -e 's/^"//' -e 's/"$//') <<< "$currentInstance")
+
+        imageId=$((grep "\"ImageId\"" | sed 's: ::g' | sed 's/,$//' | uniq | head -1 | cut -d ':' -f 2 | sed -e 's/^"//' -e 's/"$//') <<< "$currentInstance")
+
+        environmentLineNumber=$((grep -n "\"environment\"" | cut -d : -f 1) <<< "$currentInstance")
+        instanceEnvironment=$((head -n $((environmentLineNumber + 1)) | tail -n 1 | sed 's: ::g' | sed 's/,$//' | uniq | head -1 | cut -d ':' -f 2 | sed -e 's/^"//' -e 's/"$//') <<< "$currentInstance")
+
+        echo "\n\n\n########################################\nimageId:          $imageId\nenvironment:      $instanceEnvironment\nprivateIpAddress: $privateIpAddress\ninstanceType:     $instanceType\n########################################\n\n\n"
+        scp -i ~/.keys/$session-web-$instanceEnvironment.pem $fileFrom ubuntu@$privateIpAddress:$pathTo
+    done
+}
+
 function vpn_connect {
     /usr/bin/env osascript <<-EOF
 tell application "System Events"
