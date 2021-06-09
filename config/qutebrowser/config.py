90ea77dec871601bfbd9350674bde3e3e99de035
Oriol Garcia <loirog@gmail.com>
2021-06-10 01:34:18 +0200

    Updates qutebrower configuration

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -41,9 +41,14 @@ config.bind("xs", "config-cycle statusbar.show always switching")
 config.bind("xt", "config-cycle tabs.show always switching")
 config.bind("xx", "config-cycle statusbar.show always switching ;; config-cycle tabs.show always switching")
 
+config.bind("ym", "yank inline '[{title}] - {url:pretty}'")
+
 config.confirm_quit = "multiple-tabs"
 
 c.completion.cmd_history_max_items = 1024
+
+c.url.start_pages = ["about:blank"]
+c.url.default_page = "about:blank"
 
 c.content.autoplay = False
 c.content.cookies.accept = "no-unknown-3rdparty"
@@ -58,8 +63,14 @@ c.tabs.last_close = "close"
 c.tabs.title.format = "{perc}{audio}{index}: {current_title}"
 c.tabs.title.format_pinned = "{index}: {host}"
 
+c.colors.tabs.selected.even.bg = "#ff5555"
+c.colors.tabs.selected.odd.bg = "#ff5555"
+c.colors.tabs.pinned.selected.even.bg = "#ff5555"
+c.colors.tabs.pinned.selected.odd.bg = "#ff5555"
+
 c.url.searchengines["g"] = "https://google.com/search?hl=en&q={}"
 c.url.searchengines["yt"] = "https://youtube.com/results?search_query={}"
 c.url.searchengines["re"] = "https://reddit.com/r/{}"
+c.url.searchengines["tr"] = "https://translate.google.com/?sl=es&tl=en&text={}&op=translate"
 
 # c.window.hide_decoration = True
